fe4047c405eed2f99095e40b0b04291f5fc2f4c2
AhmadAmin0007404 <aminnaufal150@gmail.com>
2022-12-09 02:41:25 +0800

    New Index

--- index.js
+++ index.js
@@ -785,8 +785,7 @@ try {
           console.log(color("Silahkan scan qr code dibawah ini..", "yellow"));
         }
         if (connection === "close") {
-          console.log(DisconnectReason.timedOut)
-          connectToWhatsApp();
+          //connectToWhatsApp();
         }
         if (connection === "connecting") {
           console.log(color("Connecting..", "yellow"));
